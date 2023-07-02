6f2d81d1cbfcdb08b5399b28f86d1540124442b2
iqbalsz <94057104+iqbalsz@users.noreply.github.com>
2023-07-02 17:22:23 +0700

    adding random username function

--- cypress/e2e/M Arif Iqbal Hidayatullah/SignUp.cy.js
+++ cypress/e2e/M Arif Iqbal Hidayatullah/SignUp.cy.js
@@ -8,14 +8,20 @@ describe('Sign Up Test', () => {
   it('TC-SignUp-01 -- Verify Sign Up Successfully', () => {
     cy.get('#signin2').click() // Klik menu header Sign Up
     cy.wait(400)
-    cy.get(LoginPage.reg_uname).type('dummyscreenshotneww') // Input Username/Email
+
+    // Random uname function
+    const uuid = () => Cypress._.random(0, 1e6)
+    const id = uuid()
+    const testname = `testname${id}`
+
+    cy.get(LoginPage.reg_uname).type(testname) // Input Username/Email
     cy.get(LoginPage.reg_password).type(inputan.vld_password) // Input Password
     cy.get('[onclick="register()"]').click() // klik tombol Sign Up
     cy.on('window:alert', (t)=>{
       expect(t).to.contains('Sign up successful.')
     })
-    cy.wait(1000)
-    cy.screenshot({capture:'runner'})
+    // cy.wait(1000)
+    // cy.screenshot({capture:'runner'})
   })
 
   it('TC-SignUp-02 -- Verify Sign Up Failed -- Taken Username', () => {
